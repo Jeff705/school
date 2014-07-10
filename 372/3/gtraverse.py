0dcc440e3e24ddb1f4cecf73e2e38274898ce12f
ineptjedibob <ineptjedibob@ubuntu.(none)>
2014-07-10 15:49:41 -0400

    fuck yeah it works

--- 372/3/gtraverse.py
+++ 372/3/gtraverse.py
@@ -1,10 +1,9 @@
-#TODO: DETERMINE WHERE SEARCH ACTUALLY BELONGS AND PUT IT THERE
-
-
 import os
 import sys
+import Tkinter as tk
 
 GEOMETRY = '640x480+400+400'
+
 #Observable was shamelessly taken from the Toy MVC in the project spec
 class Observable:
   def __init__(self, initValue=None):
@@ -35,21 +34,75 @@ class Model(object):
 
   def __init__(self):
     self.hits = Observable([])
-    self.entry = Observable("") 
-    self.directory = os.getcwd()
+    self.directory = Observable(os.getcwd())
 
   def clear(self):
-    self.set([])
+    self.setHits([])
 #UNUSED AS OF YET, WILL BE MADE FUNCTIONAL LATER
   def newDir(self, newDirectory):
     self.directory = newDirectory
 
-  def getEntry(self)
-    return self.entry.get()
+  def setHits(self, newHits):
+    self.hits.set(newHits)
 
-  def search(self, part):
-    self.clear()
-    subdirs = [self.directory]
+  def getDir(self):
+    return self.directory.get()
+
+class View(tk.Toplevel):
+  def __init__(self, master):
+    tk.Toplevel.__init__(self, master)
+    self.protocol('WM_DELETE_WINDOW', self.master.destroy)
+    self.geometry(GEOMETRY)
+    self.bind("<Escape>", lambda event: self.quit())
+    label = tk.Label(self, text = "Graphical file locator")
+    label.pack()
+    self.fileList = tk.Listbox(self, width = 60, height = 20)
+    self.fileList.pack()
+    bottomFrame = tk.Frame(self, height = 8)
+    bottomFrame.pack(side = tk.BOTTOM)
+    self.entryBox = tk.Entry(bottomFrame, width = 32)
+    self.entryBox.insert(0,"<Enter search term here>")
+    self.entryBox.bind("<Button-1>", self.clearEntry)
+    self.entryBox.pack(side = tk.LEFT)
+    self.clearButton = tk.Button(bottomFrame, text = "Clear")
+    self.clearButton.config(width = 10, height = 4)
+    self.clearButton.pack(side = tk.LEFT, padx = 10)
+    self.searchButton = tk.Button(bottomFrame, text = "Search")
+    self.searchButton.config(width = 10, height = 4)
+    self.searchButton.pack(side = tk.LEFT)
+
+  def updateHits(self, newHits):
+    self.fileList.delete(0,tk.END)
+    for h in newHits:
+      self.fileList.insert(tk.END,h)
+    
+  def clearEntry(self, event):
+    self.entryBox.delete(0,tk.END)
+
+  def getEntry(self):
+    return self.entryBox.get()
+
+class Controller(object):
+  def __init__(self, root):
+    self.model = Model()
+    self.model.hits.addCallback(self.hitsChanged)
+    self.view = View(root)
+    self.view.clearButton.config(command = self.clear)
+    self.view.searchButton.config(command = self.searchModel)
+  
+  def hitsChanged(self, hits):
+    self.view.updateHits(hits)
+
+  def entryChanged(self,entry):
+    pass
+
+  def clear(self):
+    self.model.clear()
+    
+  def searchModel(self):
+    self.model.clear()
+    part = self.view.getEntry()
+    subdirs = [self.model.getDir()]
     tempHits = []
     while subdirs:
       dirToSearch = subdirs.pop()
@@ -64,52 +117,12 @@ class Model(object):
         if part in i:
 	  tempHits.append(toPush)
       
-      self.hits.set(tempHits)
-
-class View(tk.Toplevel):
-  def __init__(self, root):
-    tk.Toplevel.__init__(self, master)
-    self.protocol('WM_DELETE_WINDOW', self.master.destroy)
-    self.geometry(GEOMETRY)
-    self.bind("<Escape>", lambda event: self.quit())
-    label = tk.Label(self, text = "Graphical file locator")
-    label.pack()
-    self.fileList = tk.Listbox(self)
-    self.fileList.pack()
-    bottomFrame = tk.Frame(self, height = 50)
-    bottomFrame.pack(side = tk.BOTTOM)
-    self.entryBox = tk.Entry(bottomFrame, width = 320)
-    self.entryBox.pack(side = tk.LEFT)
-    self.clearButton = tk.Button(bottomFrame, text = "Clear")
-    self.clearButton.config(width = 100, height = 40)
-    self.clearButton.pack(side = tk.LEFT, padx = 10)
-    self.searchButton = tk.Button(bottomFrame, text = "Search")
-    self.searchButton.config(width = 100, height = 40)
-    self.searchButton.pack(side = tk.LEFT, padx = 10)
-    
-
-class Controller(object):
-  def __init__(self, root):
-    self.model = Model()
-    self.model.hits.addCallback(self.hitsChanged)
-    self.model.entry.addCallback(self.entryChanged)
-    self.view = View(root)
-    self.view.clearButton.config(command = self.clear)
-    self.view.searchButton.config(command = self.searchModel)
-  
-  def hitsChanged(self, hits):
-    self.view.updateHits(hits)
-
-  def entryChanged(self,entry):
-    pass
-
-  def clear(self)
-    self.model.clear()
-
-  def searchModel(self, part)
-    self.model.search(self,part)
+      self.view.updateHits(tempHits)
 
 
 
 if __name__ == '__main__':
-  pass
+  root = tk.Tk()
+  root.withdraw()
+  main = Controller(root)
+  root.mainloop()
